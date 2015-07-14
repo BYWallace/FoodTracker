1667b42b281bff2fb9e483c8c08c9c672ce38464
Brett Wallace <bwallace90@gmail.com>
2015-07-14 16:55:08 -0400

    Set up DetailViewController

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Brett Wallace on 7/14/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func eatItBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
+}
