c6de788388f4982fddb6ddf61f206f17b11e2ba3
Brett Wallace <bwallace90@gmail.com>
2015-07-15 23:49:31 -0400

    Show unfiltered suggested foods in tableView

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -42,11 +42,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // Mark - UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        
+        if self.searchController.active {
+            return self.filteredSuggestedSearchFoods.count
+        }
+        else {
+            return self.suggestedSearchFoods.count
+        }
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
+        var foodName:String
+        
+        if self.searchController.active {
+            foodName = filteredSuggestedSearchFoods[indexPath.row]
+        }
+        else {
+            foodName = suggestedSearchFoods[indexPath.row]
+        }
+        
+        cell.textLabel?.text = foodName
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
     }
     
     // Mark - UISearchResultsUpdating
