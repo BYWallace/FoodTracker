3029c6b8c56b52536b5f5539dacf7edc8e8fdf3c
Brett Wallace <bwallace90@gmail.com>
2015-08-01 23:17:50 -0400

    Initial setup of HealthStoreConstant

---
+++ FoodTracker/HealthStoreConstant.swift
@@ -0,0 +1,14 @@
+//
+//  HealthStoreConstant.swift
+//  FoodTracker
+//
+//  Created by Brett Wallace on 8/1/15.
+//  Copyright (c) 2015 Brett Wallace. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthStoreConstant {
+    let healthStore: HKHealthStore? = HKHealthStore()
+}
